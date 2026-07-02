993a3fee389b3eb6e47d146f5347f2e9c4e2ed13
MiladYSF <218088250+MiladYSF@users.noreply.github.com>
2026-07-02 17:35:20 +0200

    add remote state backend

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "miladtfstate"
+    container_name       = "tfstate"
+    key                  = "cicd.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
